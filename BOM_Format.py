31334d696b12d81237324decc1c8ec53b732b224
swiftvic <victor.auyeung@gmail.com>
2019-02-11 12:23:33 -0500

    renamed files to hide identity

--- BOM_Format.py
+++ BOM_Format.py
@@ -1,4 +1,16 @@
 # BOM formating 
+# Original BOM 9010242321 now 123240109
+import openpyxl
+
+def main():
+
+    filepath_old = "a_20180412_064608761.xlsx"
+    filepath_new = "a_20181029_024744353.xlsx"
+
+    
+
+    wb = openpyxl.load_workbook(filepath)
+    pass
 
 if '__name__' == '__main__':
-    pass
+    main()
